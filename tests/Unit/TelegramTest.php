8ba35f91f73d5feb58666f54016ad01eb43bc6b7
Murilo Perosa <perosamurilo@gmail.com>
2021-11-28 15:44:00 -0300

    first commit

--- tests/Unit/TelegramTest.php
+++ tests/Unit/TelegramTest.php
@@ -4,8 +4,4 @@ use MuriloPerosa\TelegramReports\Telegram;
 
 test('Token is required.', function () 
 {
-    // $report = new Telegram('2103347866:AAHBUa8wX4oq760RHo202w3vulAqrMitYtY', '1087259335');
-    // // $res = $report->report(Telegram::REPORT_LEVEL_DEBUG, '<strong>Hello World</strong>');
-    // $res = $report->reportException(new Exception('Moio! Deu BO!', 112), true);
-    // expect($res)->toBeObject();
 });
